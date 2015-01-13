33991b47197674f3927a078a584d5ecfba0cf5c3
Vgr E.Barry <vgr255@live.ca>
2015-01-12 19:04:47 -0500

    Added a lot of spells.

--- pyrpg.py
+++ pyrpg.py
@@ -74,14 +74,14 @@ class AttackFormula(DamageTypes):
             self.strength = strength
 
         def damage(self, attacker, target):
-            return int(attacker.cur_hp * (self.strength / 32))
+            return int(target.cur_hp * (self.strength / 32))
 
     class MaxHPFormula:
         def __init__(self, strength):
             self.strength = strength
 
         def damage(self, attacker, target):
-            return int(attacker.max_hp * (self.strength / 32))
+            return int(target.max_hp * (self.strength / 32))
 
     class FixedDamage:
         def __init__(self, strength):
@@ -97,7 +97,7 @@ class AttackFormula(DamageTypes):
         def damage(self, attacker, target):
             return _recovery(attacker, target)
 
-class AttackAttributes:
+class AttackGenerator(AttackFormula):
     def __init__(self, **kwargs):
         # Target settings
         self.enable_selection = True
@@ -113,6 +113,8 @@ class AttackAttributes:
         # Attack properties
         self.element = None
         self.type = PHYSICAL
+        self.attack_category = None
+        self.attack_formula = NoDamage
         self.affect_mp = False
         self.drain_hp = False
         self.drain_mp = False
@@ -120,11 +122,79 @@ class AttackAttributes:
         self.only_dead = False
         self.reflectable = False
         self.no_retarget = False
-        self.allow_crit = True
+        self.allow_crit = False
         self.additional_effect = None
-        self.status_effect = ()
-        self.status_chance = 0
-        self.strength = 0
-        self.mp_cost = 0
+        self.status_effects = ()
+        self.status_modify = STATUS_ADD
+        self.status_chance = 0 # up to 255 (base 16)
+        self.strength = 0 # base 10
+        self.mp_cost = 0 # base 10
+        self.hit_rate = 0 # up to 255 (base 16)
 
         self.__dict__.update(kwargs)
+
+        self._attack_formula = self.attack_formula(self.strength)
+
+# Black Magic
+
+Fire = AttackGenerator(element=FIRE, type=MAGICAL, attack_category=BLACK_MAGIC, attack_formula=StrengthAttackLevel2, reflectable=True, strength=11, mp_cost=4, hit_rate=100)
+Fira = AttackGenerator(element=FIRE, type=MAGICAL, attack_category=BLACK_MAGIC, attack_formula=StrengthAttackLevel2, reflectable=True, strength=24, mp_cost=22, hit_rate=100)
+Firaga = AttackGenerator(element=FIRE, type=MAGICAL, attack_category=BLACK_MAGIC, attack_formula=StrengthAttackLevel2, reflectable=True, strength=70, mp_cost=52, hit_rate=100)
+
+Blizzard = AttackGenerator(element=ICE, type=MAGICAL, attack_category=BLACK_MAGIC, attack_formula=StrengthAttackLevel2, reflectable=True, strength=11, mp_cost=4, hit_rate=100)
+Blizzara = AttackGenerator(element=ICE, type=MAGICAL, attack_category=BLACK_MAGIC, attack_formula=StrengthAttackLevel2, reflectable=True, strength=24, mp_cost=22, hit_rate=100)
+Blizzaga = AttackGenerator(element=ICE, type=MAGICAL, attack_category=BLACK_MAGIC, attack_formula=StrengthAttackLevel2, reflectable=True, strength=70, mp_cost=52, hit_rate=100)
+
+Thunder = AttackGenerator(element=THUNDER, type=MAGICAL, attack_category=BLACK_MAGIC, attack_formula=StrengthAttackLevel2, reflectable=True, strength=11, mp_cost=4, hit_rate=100)
+Thundara = AttackGenerator(element=THUNDER, type=MAGICAL, attack_category=BLACK_MAGIC, attack_formula=StrengthAttackLevel2, reflectable=True, strength=24, mp_cost=22, hit_rate=100)
+Thundaga = AttackGenerator(element=THUNDER, type=MAGICAL, attack_category=BLACK_MAGIC, attack_formula=StrengthAttackLevel2, reflectable=True, strength=70, mp_cost=52, hit_rate=100)
+
+Aero = AttackGenerator(element=WIND, type=MAGICAL, attack_category=BLACK_MAGIC, attack_formula=StrengthAttackLevel2, reflectable=True, strength=11, mp_cost=4, hit_rate=100)
+Aera = AttackGenerator(element=WIND, type=MAGICAL, attack_category=BLACK_MAGIC, attack_formula=StrengthAttackLevel2, reflectable=True, strength=24, mp_cost=22, hit_rate=100)
+Aeroga = AttackGenerator(element=WIND, type=MAGICAL, attack_category=BLACK_MAGIC, attack_formula=StrengthAttackLevel2, reflectable=True, strength=70, mp_cost=52, hit_rate=100)
+
+# Red Magic
+
+Shake = AttackGenerator(element=EARTH, type=MAGICAL, attack_category=RED_MAGIC, attack_formula=StrengthAttackLevel2, reflectable=True, strength=14, mp_cost=6, hit_rate=100)
+Quake = AttackGenerator(element=EARTH, type=MAGICAL, attack_category=RED_MAGIC, attack_formula=StrengthAttackLevel2, reflectable=True, strength=27, mp_cost=28, hit_rate=100)
+Seism = AttackGenerator(element=EARTH, type=MAGICAL, attack_category=RED_MAGIC, attack_formula=StrengthAttackLevel2, reflectable=True, strength=74, mp_cost=68, hit_Rate=100)
+
+Water = AttackGenerator(element=WATER, type=MAGICAL, attack_category=RED_MAGIC, attack_formula=StrengthAttackLevel2,reflectable=True,  strength=14, mp_cost=6, hit_rate=100)
+Wave = AttackGenerator(element=WATER, type=MAGICAL, attack_category=RED_MAGIC, attack_formula=StrengthAttackLevel2, reflectable=True, strength=27, mp_cost=28, hit_rate=100)
+Flood = AttackGenerator(element=WATER, type=MAGICAL, attack_category=RED_MAGIC, attack_formula=StrengthAttackLevel2, reflectable=True, strength=74, mp_cost=68, hit_rate=100)
+
+Bio = AttackGenerator(element=POISON, type=MAGICAL, attack_category=RED_MAGIC, attack_formula=StrengthAttackLevel2, reflectable=True, strength=10, mp_cost=8, status_effects=(VIRUS,), status_chance=32, hit_rate=100)
+Disease = AttackGenerator(element=POISON, type=MAGICAL, attack_category=RED_MAGIC, attack_formula=StrengthAttackLevel2, reflectable=True, strength=24, mp_cost=36, status_effects=(VIRUS,), status_chance=49, hit_rate=100)
+Virus = AttackGenerator(element=POISON, type=MAGICAL, attack_category=RED_MAGIC, attack_formula=StrengthAttackLevel2, reflectable=True, strength=68, mp_cost=80, status_effects=(VIRUS,), status_chance=98, hit_rate=100)
+
+Dia = AttackGenerator(element=HOLY, type=MAGICAL, attack_category=RED_MAGIC, attack_formula=StrengthAttackLevel2, reflectable=True, strength=10, mp_cost=8, hit_rate=100)
+Pure = AttackGenerator(element=HOLY, type=MAGICAL, attack_category=RED_MAGIC, attack_formula=StrengthAttackLevel2, reflectable=True, strength=24, mp_cost=36, hit_rate=100)
+White = AttackGenerator(element=HOLY, type=MAGICAL, attack_category=RED_MAGIC, attack_formula=StrengthAttackLevel2, reflectable=True, strength=68, mp_cost=80, hit_rate=100)
+
+# White Magic
+
+Cure = AttackGenerator(element=RESTORATIVE, type=MAGICAL, attack_category=WHITE_MAGIC, attack_formula=StrengthAttackLevel3, reflectable=True, strength=5, mp_cost=5, hit_rate=120)
+Cura = AttackGenerator(element=RESTORATIVE, type=MAGICAL, attack_category=WHITE_MAGIC, attack_formula=StrengthAttackLevel3, reflectable=True, strength=34, mp_cost=24, hit_rate=120)
+Curaga = AttackGenerator(element=RESTORATIVE, type=MAGICAL, attack_category=WHITE_MAGIC, attack_formula=StrengthAttackLevel3, reflectable=True, strength=130, mp_cost=64, hit_rate=120)
+
+# Blue Magic
+
+Gravity = AttackGenerator(element=GRAVITY, type=MAGICAL, attack_category=BLUE_MAGIC, attack_formula=CurrentHPFormula, reflectable=True, strength=8, mp_cost=14, hit_rate=75)
+Gravira = AttackGenerator(element=GRAVITY, type=MAGICAL, attack_category=BLUE_MAGIC, attack_formula=CurrentHPFormula, reflectable=True, strength=16, mp_cost=33, hit_rate=75)
+Graviga = AttackGenerator(element=GRAVITY, type=MAGICAL, attack_category=BLUE_MAGIC, attack_formula=CurrentHPFormula, reflectable=True, strength=24, mp_cost=48, hit_rate=75)
+
+# Purple Magic
+
+Hell = AttackGenerator(element=FIRE, type=MAGICAL, attack_category=PURPLE_MAGIC, attack_formula=StrengthAttackLevel2, reflectable=True, strength=116, mp_cost=96, hit_rate=100)
+Freeze = AttackGenerator(element=ICE, type=MAGICAL, attack_category=PURPLE_MAGIC, attack_formula=StrengthAttackLevel2, reflectable=True, strength=116, mp_cost=96, hit_rate=100)
+Electrocute = AttackGenerator(element=THUNDER, type=MAGICAL, attack_category=PURPLE_MAGIC, attack_formula=StrengthAttackLevel2, reflectable=True, strength=116, mp_cost=96, hit_rate=100)
+Tornado = AttackGenerator(element=WIND, type=MAGICAL, attack_category=PURPLE_MAGIC, attack_formula=StrengthAttackLevel2, reflectable=True, strength=116, mp_cost=96, hit_rate=100)
+
+Stone = AttackGenerator(element=EARTH, type=MAGICAL, attack_category=PURPLE_MAGIC, attack_formula=StrengthAttackLevel2, reflectable=True, strength=120, mp_cost=108, hit_rate=100)
+Tsunami = AttackGenerator(element=WATER, type=MAGICAL, attack_category=PURPLE_MAGIC, attack_formula=StrengthAttackLevel2, reflectable=True, strength=120, mp_cost=108, hit_rate=100)
+Pandemic = AttackGenerator(element=POISON, type=MAGICAL, attack_category=PURPLE_MAGIC, attack_formula=StrengthAttackLevel2, reflectable=True, strength=112, mp_cost=128, hit_rate=100)
+Holy = AttackGenerator(element=HOLY, type=MAGICAL, attack_category=PURPLE_MAGIC, attack_formula=StrengthAttackLevel2, reflectable=True, strength=112, mp_cost=128, hit_rate=100)
+
+Curaja = AttackGenerator(element=RESTORATIVE, type=MAGICAL, attack_category=PURPLE_MAGIC, attack_formula=StrengthAttackLevel3, reflectable=True, strength=182, mp_cost=84, hit_rate=100)
+
+Gravija = AttackGenerator(element=GRAVITY, type=MAGICAL, attack_category=PURPLE_MAGIC, attack_formula=CurrentHPFormula, reflectable=True, strength=30, mp_cost=62, hit_rate=75)
